acb5b845f8863ef6ef95e2b335f53a3748b8ac14
Oleg Kl <oklyavlin@tourmalinecore.com>
2022-12-22 17:58:34 +0500

    build: add reading configuration from dev environment

--- EmailSender.Api/Program.cs
+++ EmailSender.Api/Program.cs
@@ -1,10 +1,36 @@
 using EmailSender.Application;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 var configuration = builder.Configuration;
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    var env = hostingContext.HostingEnvironment;
+
+    var reloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", true);
+
+    config.AddJsonFile("appsettings.json", true, reloadOnChange)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, reloadOnChange)
+        .AddJsonFile("appsettings.Active.json", true, reloadOnChange);
+
+    if (env.IsDevelopment() && !string.IsNullOrEmpty(env.ApplicationName))
+    {
+        var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+
+        config.AddUserSecrets(appAssembly, true);
+    }
+
+    config.AddEnvironmentVariables();
+
+    if (args != null)
+    {
+        config.AddCommandLine(args);
+    }
+});
 
 builder.Services.AddApplication(configuration);
 
