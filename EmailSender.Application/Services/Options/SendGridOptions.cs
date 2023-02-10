b62a661ca910e0c0789827eafddfbd260b8f8da8
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-02-10 12:44:29 +0500

    feat: add sendgrid

---
+++ EmailSender.Application/Services/Options/SendGridOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender.Application.Services.Options
+{
+    public class SendGridOptions
+    {
+        public string SendGridAPIKey { get; set; }
+        public string SenderEmail { get; set; }
+        public string SenderName { get; set; }
+    }
+}
---
+++ EmailSender.Application/Services/SendGridEmailService.cs
@@ -0,0 +1,40 @@
+﻿using EmailSender.Application.Models;
+using Microsoft.Extensions.Options;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailSender.Application.Services.Options;
+
+namespace EmailSender.Application.Services
+{
+    public class SendGridEmailService : IEmailSender
+    {
+        private readonly SendGridOptions _mailOptions;
+        private readonly SendGridClient _client;
+
+        public SendGridEmailService(IOptions<SendGridOptions> mailOptions)
+        {
+            _mailOptions = mailOptions.Value;
+            _client = new SendGridClient(_mailOptions.SendGridAPIKey);
+        }
+
+        public async Task SendEmailAsync(MailModel mailModel)
+        {
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(_mailOptions.SenderEmail, _mailOptions.SenderName),
+                Subject = mailModel.Subject,
+                PlainTextContent = mailModel.Body,
+                HtmlContent = $"<div>{mailModel.Body}</div>",
+            };
+
+            msg.AddTo(mailModel.To);
+
+            await _client.SendEmailAsync(msg);
+        }
+    }
+}
