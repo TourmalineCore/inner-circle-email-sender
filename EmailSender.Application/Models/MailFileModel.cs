9bbb3927dad034f37c952a24132a34e595e6d92c
Dmitriy Myakotin <demon4301@gmail.com>
2024-03-04 11:10:31 +0500

    feat(SendDocuments): #8693yhxv7: add request for send documents

---
+++ EmailSender.Application/Models/MailFileModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EmailSender.Application.Models
+{
+    public class MailFileModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
